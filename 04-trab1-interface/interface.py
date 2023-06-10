09f2526206f6f20e6b8c0d5876296887255eba40
Shikiyo <shikiyo@archjk>
2023-06-09 22:52:51 -0300

    Interface trab1 v2
    Remove unused stuff
    Rename Tag -> Topic
    Replace pass with assert False, "TO BE IMPLEMENTED"
    Note about subscribe_all semantics

--- 04-trab1-interface/interface.py
+++ 04-trab1-interface/interface.py
@@ -1,10 +1,9 @@
 from __future__ import annotations
 
 from typing import Callable, Optional, Tuple, TypeAlias
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
 import rpyc # type: ignore
-# import time
 
 UserId: TypeAlias = int
 
@@ -15,56 +14,63 @@ class UserInfo:
     user_id: UserId
     user_name: str
 
-Tag: TypeAlias = str
+Topic: TypeAlias = str
 
 # Isso é para ser tipo uma struct
 # Frozen diz que os campos são read-only
 @dataclass(frozen=True, kw_only=True, slots=True)
 class Content:
     author: UserId
-    tag: Tag
+    topic: Topic
     data: str
-    # publish_timestamp: time.struct_time
 
-# Aqui pode ser uma função que recebe apenas um Tuple[Tag, Content]
+# Aqui pode ser uma função que recebe apenas um Tuple[Topic, Content]
 # ou seja:
-# FnNotify: TypeAlias = Callable[[Tuple[Tag, Content]], None]
-FnNotify: TypeAlias = Callable[[list[Tuple[Tag, Content]]], None]
+# FnNotify: TypeAlias = Callable[[Tuple[Topic, Content]], None]
+FnNotify: TypeAlias = Callable[[list[Tuple[Topic, Content]]], None]
 
 class BrokerService(rpyc.Service): # type: ignore
 
     # Não é exposed porque só o "admin" tem acesso
-    def create_tag(self, id: UserId, tagname: str) -> Tag:
-        pass
+    def create_topic(self, id: UserId, topicname: str) -> Topic:
+        assert False, "TO BE IMPLEMENTED"
 
     # Handshake
 
     def exposed_login(self, username: str) -> Optional[UserId]:
-        pass
+        assert False, "TO BE IMPLEMENTED"
 
     # Query operations
 
     def exposed_get_user_info(self, id: UserId) -> UserInfo:
-        pass
+        assert False, "TO BE IMPLEMENTED"
 
-    def exposed_list_tags(self) -> list[Tag]:
-        pass
+    def exposed_list_topics(self) -> list[Topic]:
+        assert False, "TO BE IMPLEMENTED"
 
     # Publisher operations
 
-    def exposed_publish(self, id: UserId, tag: Tag, data: str) -> None:
-        pass
+    def exposed_publish(self, id: UserId, topic: Topic, data: str) -> None:
+        assert False, "TO BE IMPLEMENTED"
 
     # Subscriber operations
 
-    def exposed_subscribe_to(self, id: UserId, tag: Tag, callback: FnNotify) -> Optional[FnNotify]:
-        pass
+    def exposed_subscribe_to(self, id: UserId, topic: Topic, callback: FnNotify) -> Optional[FnNotify]:
+        assert False, "TO BE IMPLEMENTED"
 
+    def exposed_unsubscribe_to(self, id: UserId, topic: Topic) -> Optional[FnNotify]:
+        assert False, "TO BE IMPLEMENTED"
+
+    # Nota: A semântica do subscribe_all não foi definida, opções:
+    # 0 - Jogar essa função fora (e unsubscribe_all)
+    # 1 - Se um topico for criado depois, deve inscrever o User nele
+    # 2 - Se um topico for criado depois, não deve inscrever o User nele
+    #
+    # a: nos casos (0) e (2) fazem sentido se inscrever no "evento" de "novo tópico"
     def exposed_subscribe_all(self, id: UserId, callback: FnNotify) -> Optional[FnNotify]:
-        pass
+        assert False, "TO BE IMPLEMENTED"
 
-    def exposed_unsubscribe_to(self, id: UserId, tag: Tag) -> FnNotify:
-        pass
-
+    # Nota: não sei como deve ser o retorno dessa função,
+    # talvez: dict[Topic, Optional[FnNotify]]
     def exposed_unsubscribe_all(self, id: UserId) -> FnNotify:
-        pass
+        assert False, "TO BE IMPLEMENTED"
